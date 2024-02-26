0c6c897b50fd7be10720828e9efaba1cc274316c
sejongdeveloper <sejongdeveloper@gmail.com>
2024-02-26 23:34:47 +0900

    gateway 프로젝트 생성

---
+++ gateway/src/test/java/com/study/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
