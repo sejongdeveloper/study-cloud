463c9f07b284500d0802a2f4ba3a191cd8bc6bf7
sejongdeveloper <sejongdeveloper@gmail.com>
2024-02-27 16:34:29 +0900

    todo api 추가

---
+++ todo/src/main/java/com/study/todo/controller/TodoApiController.java
@@ -0,0 +1,15 @@
+package com.study.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/todos")
+public class TodoApiController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "todo test";
+    }
+}
