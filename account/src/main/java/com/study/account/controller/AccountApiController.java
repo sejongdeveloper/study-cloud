ce47525011c6dcd1cbd4ef6df329ad21f617332a
sejongdeveloper <sejongdeveloper@gmail.com>
2024-02-27 15:59:24 +0900

    api 경로 변경

--- account/src/main/java/com/study/account/controller/AccountApiController.java
+++ account/src/main/java/com/study/account/controller/AccountApiController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountApiController {
     private final AppProperties appProperties;
 
-    @GetMapping("/hello")
-    public String hello() {
+    @GetMapping("/message")
+    public String getMessage() {
         return appProperties.getMessage();
     }
 
