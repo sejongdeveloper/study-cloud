cdf33f9d23e65b6616613965820e2f44c671a9e2
sejongdeveloper <sejongdeveloper@gmail.com>
2024-02-27 15:04:58 +0900

    hello api > git config 연동

--- account/src/main/java/com/study/account/controller/AccountApiController.java
+++ account/src/main/java/com/study/account/controller/AccountApiController.java
@@ -1,14 +1,20 @@
 package com.study.account.controller;
 
+import com.study.account.properties.AppProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
+@RequiredArgsConstructor
 public class AccountApiController {
+    private final AppProperties appProperties;
+
     @GetMapping("/hello")
     public String hello() {
-        return "account project hello!";
+        return appProperties.getMessage();
     }
+
 }
