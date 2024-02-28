5d8f8bd87b716a2119ea6741392232b8314a2f7c
sejongdeveloper <sejongdeveloper@gmail.com>
2024-02-28 14:21:51 +0900

    prometheus 적용

--- account/src/main/java/com/study/account/controller/AccountApiController.java
+++ account/src/main/java/com/study/account/controller/AccountApiController.java
@@ -2,6 +2,7 @@ package com.study.account.controller;
 
 import com.study.account.properties.AppProperties;
 import com.study.account.service.AccountService;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,24 +17,28 @@ public class AccountApiController {
     private final AppProperties appProperties;
     private final AccountService accountService;
 
+    @Timed(value = "account.message", longTask = true)
     @GetMapping("/message")
     public String getMessage() {
         log.info("/message");
         return appProperties.getMessage();
     }
 
+    @Timed(value = "account.key", longTask = true)
     @GetMapping("/key")
     public String getKey() {
         log.info("/key");
         return appProperties.getKey();
     }
 
+    @Timed(value = "account.todo.message", longTask = true)
     @GetMapping("/todos/message")
     public String getTodoMessage() {
         log.info("/todos/message");
         return accountService.getTodoMessage();
     }
 
+    @Timed(value = "account.todo.message-fail", longTask = true)
     @GetMapping("/todos/message-fail")
     public String getTodoMessageFail() {
         log.info("/todos/message-fail");
--- todo/src/main/java/com/study/todo/controller/TodoApiController.java
+++ todo/src/main/java/com/study/todo/controller/TodoApiController.java
@@ -2,6 +2,7 @@ package com.study.todo.controller;
 
 import com.study.todo.properties.AppProperties;
 import com.study.todo.repository.TodoRepository;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +17,14 @@ public class TodoApiController {
     private final AppProperties appProperties;
     private final TodoRepository todoRepository;
 
+    @Timed(value = "todo.message", longTask = true)
     @GetMapping("/message")
     public String getMessage() {
         log.info("/message");
         return appProperties.getMessage();
     }
 
+    @Timed(value = "todo.message-count", longTask = true)
     @GetMapping("/message-count")
     public String getMessageCount() {
         log.info("/message-count");
